1663dfd7dbc63cf92a36282f8482e4066b7148ca
TheuZCoder <matheusrdsilva1@gmail.com>
2024-11-18 19:11:35 -0300

    Commit Initial, arrumando a merda

--- android/app/src/main/kotlin/com/example/portaria_condominio/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.br.portaria
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ portaria_condominio/android/app/src/main/kotlin/com/example/portaria_condominio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.portaria_condominio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
